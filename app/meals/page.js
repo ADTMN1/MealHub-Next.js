7d1dd845a43bf07a6d076c7858787a2c6d295cba
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-04-11 11:54:15 -0700

    outputing meals data and images

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,26 @@
 import Link from "next/link";
-
+import classes from "./page.module.css"
 export default function Page() {
     return (
         <div >
-            <h1>Meal Options</h1>
-            <div >
-                <Link href="/meals/post1" >
-                    Meal 1
-                </Link>
+            <header className={classes.header}>
+                <div>
+                    <h1>
+                        Delicious meals, created <span className={classes.highlight}>by you</span>
+                    </h1>
+                    <p>
+                        choose your favorite recipe and cook it yourself.It is easy and fun!
+                    </p>
+                    <p className={classes.cta}>
+                        <Link href="/meals/share">Share your Favorite Recipe</Link>
+                    </p>
+                </div>
+            </header>
 
-            </div>
+            <main className={classes.main}>
+                <mealsGrid meals={[]} />
+            </main>
+
         </div>
     );
 }
