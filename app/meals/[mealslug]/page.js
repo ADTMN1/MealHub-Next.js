0f13e1054ba893c4e79dcd14921c2329b8e9f52a
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-04-12 00:40:04 -0700

    adding mealDetails via dynamic route

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,10 +1,37 @@
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import classes from "./page.module.css"
 
-export default function page() {
+
+
+export default function MealDetailsPage({ params }) {
+
+    const meal = getMeal(params.mealslug)
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
     return (
-        <div>
-
-            <d><h1>meal post</h1></d>
-
-        </div>
-    )
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
+                ></p>
+            </main>
+        </>
+    );
 }
