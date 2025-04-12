c2edfb854621bbb171e3be748c6b8fda87916262
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-04-12 13:41:19 -0700

    managing the form submission using useFormStatus

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from '@/component/meals/imagePicker';
 import classes from './page.module.css';
 import { ShareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/component/meals/meals-form-submit';
 
 export default function ShareMealPage() {
 
@@ -43,7 +44,7 @@ export default function ShareMealPage() {
                     </p>
                     <ImagePicker label="Your image" name="image" />
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
