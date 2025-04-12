a7362477235487088d4667b9f6d599163067e7d4
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-04-12 12:47:36 -0700

    storing uploaded images

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,42 +1,36 @@
-import Link from "next/link";
-import classes from "./page.module.css"
-import getMeals from "@/lib/meals";
-import MealsGrid from "@/component/meals/meals-grid";
-import { Suspense } from "react";
+import { Suspense } from 'react';
+import Link from 'next/link';
+
+import classes from './page.module.css';
+import MealsGrid from '@/component/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
 async function Meals() {
     const meals = await getMeals();
-    return <MealsGrid meals={meals} />
 
+    return <MealsGrid meals={meals} />;
 }
 
-
-export default function Page() {
-
-
+export default function MealsPage() {
     return (
-        <div >
+        <>
             <header className={classes.header}>
-                <div>
-                    <h1>
-                        Delicious meals, created <span className={classes.highlight}>by you</span>
-                    </h1>
-                    <p>
-                        choose your favorite recipe and cook it yourself.It is easy and fun!
-                    </p>
-                    <p className={classes.cta}>
-                        <Link href="/meals/share">Share your Favorite Recipe</Link>
-                    </p>
-                </div>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favorite Recipe</Link>
+                </p>
             </header>
-
             <main className={classes.main}>
                 <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
                     <Meals />
                 </Suspense>
-
             </main>
-
-        </div>
+        </>
     );
 }
