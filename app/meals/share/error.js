b0a59758841279cd4186f26151810e3ec0ef500c
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-04-12 15:20:01 -0700

    server action validation

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function error({ error }) {
+    return (
+        <main className="error">
+            <h1>An error Occurred! </h1>
+            <p>Failed to create meal.</p>
+        </main>
+    )
+}
