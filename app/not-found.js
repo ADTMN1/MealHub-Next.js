257a1ca8da25b85b64633b54e9cab86669dfa614
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-04-11 23:13:23 -0700

    handling 'not found' and 'errors'

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+
+"use client"
+
+export default function error({ error }) {
+    return (
+        <main className="not-found">
+            <h1>Page not found </h1>
+            <p>unfortunately, we could not find the requested page or resources.</p>
+        </main>
+    )
+}
